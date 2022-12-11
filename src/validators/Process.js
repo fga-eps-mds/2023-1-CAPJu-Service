ba0444f8ffc108c326728499fc4a1af5b811bbe0
SamuelGSouza <samuelgomes318@gmail.com>
2022-12-11 17:48:26 -0300

    44-Adicionando validação da observação

--- src/validators/Process.js
+++ src/validators/Process.js
@@ -28,3 +28,9 @@ export const NextStageValidator = Joi.object({
   stageIdFrom: Joi.string().required(),
   observation: Joi.string().allow(null, ""),
 });
+
+export const updateObservation = Joi.object({
+  processId: Joi.string().required(),
+  stageIdFrom: Joi.string().required(),
+  observation: Joi.string().allow(null, ""),
+});
