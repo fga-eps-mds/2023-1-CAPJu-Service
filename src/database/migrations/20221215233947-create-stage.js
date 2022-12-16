120662e9176021fb6db63e74745bc4f88bba75fc
vitorAlves7 <vitor.alves07750@outlook.com>
2022-12-15 23:00:50 -0300

    Adicionando models e novas migrations
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>

---
+++ src/database/migrations/20221215233947-create-stage.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('stage', { 
+      idStage: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(300),
+        allowNull: false,
+      },
+      duration: {
+        type: Sequelize.SMALLINT,
+        allowNull: false
+      },
+      idUnit: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'unit', key: 'idUnit' },
+        onDelete: 'RESTRICT'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.dropTable('stage');
+  }
+};
