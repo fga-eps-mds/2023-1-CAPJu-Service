3881b4eeb7418016c41b568bc4835cef41aa38cd
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2023-01-30 01:52:29 -0300

    feat: testar inserir e apagar unidade

--- src/tests/__tests__/test_unit_routes.js
+++ src/tests/__tests__/test_unit_routes.js
@@ -15,11 +15,52 @@ describe('unit endpoints', () => {
   test('new unit and list', async () => {
     const testUnit = {
       name: "Unidade Teste"
-    }
+    };
+
+    const initialUnit = {
+      name: "FGA"
+    };
 
     const newUnitResponse = await supertest(app).post("/newUnit").send(testUnit);
     expect(newUnitResponse.status).toBe(200);
 
-    //const unitsResponse = await supertest
+    const unitsResponse = await supertest(app).get("/units");
+    expect(unitsResponse.status).toBe(200);
+
+    // this unit and FGA (initial unit)
+    const expectedTestUnits = [
+      initialUnit,
+      testUnit,
+    ];
+
+    expect(unitsResponse.body.length).toBe(2);
+    expect(unitsResponse.body)
+      .toEqual(expect
+        .arrayContaining(expectedTestUnits
+          .map((expectedTestUnit) => expect
+            .objectContaining(expectedTestUnit))));
+  });
+
+  test('create unit and delete it', async () => {
+    const testUnit = {
+      name: "Unidade Teste"
+    };
+
+    const initialUnit = {
+      name: "FGA"
+    };
+
+    const newUnitResponse = await supertest(app).post("/newUnit").send(testUnit);
+    expect(newUnitResponse.status).toBe(200);
+
+
+    const deleteUnitResponse = await supertest(app).delete("/deleteunit").send({ idUnit: 2 });
+    expect(deleteUnitResponse.status).toBe(200);
+    expect(deleteUnitResponse.body).toEqual(expect.objectContaining(testUnit));
+
+    const unitsResponse = await supertest(app).get("/units");
+    expect(unitsResponse.status).toBe(200);
+    expect(unitsResponse.body.length).toBe(1);
+    expect(unitsResponse.body).toEqual(expect.arrayContaining([expect.objectContaining(initialUnit)]));
   });
 });
