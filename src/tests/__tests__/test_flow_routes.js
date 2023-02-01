78e176a44fb26f90a4a2a63e94b07070c627c040
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2023-02-01 11:35:51 -0300

    feat: testar retorno dos fluxos

--- src/tests/__tests__/test_flow_routes.js
+++ src/tests/__tests__/test_flow_routes.js
@@ -64,11 +64,40 @@ describe("flow endpoints", () => {
       },
     ];
 
+    const expectedTestFlows = testFlows.map((testFlow, index) => {
+      let stages = [];
+      for (const { from, to } of testFlow.sequences) {
+        if (!stages.includes(from)) {
+          stages.push(from);
+        }
+        if (!stages.push(to)) {
+          stages.push(to);
+        }
+      }
+      return {
+        idFlow: index + 1,
+        name: testFlow.name,
+        idUnit: testFlow.idUnit,
+        sequences: testFlow.sequences,
+        stages,
+      };
+    });
+
     for (const testFlow of testFlows) {
       const newFlowResponse = await supertest(app)
         .post("/newFlow")
         .send(testFlow);
       expect(newFlowResponse.status).toBe(200);
     }
+
+    const flowsResponse = await supertest(app).get("/flows");
+    expect(flowsResponse.status).toBe(200);
+    expect(flowsResponse.body).toEqual(
+      expect.arrayContaining(
+        expectedTestFlows.map((expectedTestFlow) =>
+          expect.objectContaining(expectedTestFlow)
+        )
+      )
+    );
   });
 });
