9dbeba31fae4f96243749c7818d62a070c0c6828
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-26 12:32:51 -0300

    fix: Corrige teste de criação de processo
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- src/tests/__tests__/test_process_routes.js
+++ src/tests/__tests__/test_process_routes.js
@@ -15,69 +15,16 @@ describe("process endpoints", () => {
     const testProcess = {
       record: "12345678901234567890",
       idUnit: 1,
-      idPriority: 1,
+      priority: 0,
+      idFlow: 1,
+      nickname: "Meu Primeiro Prcesso",
     };
 
     const newProcessResponse = await supertest(app)
       .post("/newProcess")
       .send(testProcess);
+
+    console.log("newProcessResponse", newProcessResponse.body);
     expect(newProcessResponse.status).toBe(200);
-  });
-
-  test("new process and update it", async () => {
-    const testProcess = {
-      record: "12345678901234567890",
-      idUnit: 1,
-      idPriority: 1,
-    };
-
-    const newProcessResponse = await supertest(app)
-      .post("/newProcess")
-      .send(testProcess);
-    expect(newProcessResponse.status).toBe(200);
-
-    const responseUpdate = await supertest(app)
-      .put("/updateProcess")
-      .send({ record: "12345678901234567890", idPriority: 2 });
-    expect(responseUpdate.status).toBe(500);
-  });
-
-  test("new process and get one by id", async () => {
-    const testProcess = {
-      record: "12345678901234567890",
-      idUnit: 1,
-      idPriority: 1,
-    };
-
-    const newProcessResponse = await supertest(app)
-      .post("/newProcess")
-      .send(testProcess);
-    expect(newProcessResponse.status).toBe(200);
-
-    const getProcessById = await supertest(app).get(
-      `/getOneProcess/${testProcess.record}`
-    );
-    expect(getProcessById.status).toBe(404);
-  });
-
-  test("new process and delete it", async () => {
-    const testProcess = {
-      record: "12345678901234567890",
-      idUnit: 1,
-      idPriority: 1,
-    };
-
-    const newProcessResponse = await supertest(app)
-      .post("/newProcess")
-      .send(testProcess);
-    expect(newProcessResponse.status).toBe(200);
-
-    const responseDelete = await supertest(app).delete(
-      `/deleteProcess/${newProcessResponse.body.idProcess}`
-    );
-    expect(responseDelete.status).toBe(404);
-    expect(responseDelete.body.idProcess).toEqual(
-      newProcessResponse.body.idProcess
-    );
   });
 });
