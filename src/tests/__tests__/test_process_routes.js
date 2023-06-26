3fbbf17e2d4e5d735fd616449221aba47c52c5db
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-26 12:48:48 -0300

    fix: Adiciona teste de criação e busca de processo
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- src/tests/__tests__/test_process_routes.js
+++ src/tests/__tests__/test_process_routes.js
@@ -1,6 +1,7 @@
 import { Database } from "../TestDatabase.js";
 import "sequelize";
 import supertest from "supertest";
+import Process from "../../models/Process.js";
 import { app, injectDB } from "../TestApp";
 
 describe("process endpoints", () => {
@@ -24,7 +25,41 @@ describe("process endpoints", () => {
       .post("/newProcess")
       .send(testProcess);
 
-    console.log("newProcessResponse", newProcessResponse.body);
     expect(newProcessResponse.status).toBe(200);
+  });
+
+  test("new process and search", async () => {
+    let processMock = [
+      {
+        record: "12345678901234567891",
+        idUnit: 1,
+        priority: 0,
+        idFlow: 1,
+        nickname: "Meu Primeiro Prcesso",
+      },
+      {
+        record: "12345678901234567892",
+        idUnit: 1,
+        priority: 0,
+        idFlow: 1,
+        nickname: "Meu Segundo Prcesso",
+      },
+    ];
+
+    const allProcesses = [];
+    for (const process of processMock) {
+      const newProcessResponse = await supertest(app)
+        .post("/newProcess")
+        .send(process);
+      expect(newProcessResponse.status).toBe(200);
+      expect(newProcessResponse.body.message).toEqual("Criado com sucesso!");
+      allProcesses.push(newProcessResponse.body);
+    }
+    let processes = await Process.findAll();
+    for (let index = 0; index < processes.length; index++) {
+      expect(processes[index]?.dataValues?.record).toEqual(
+        processMock[index]?.record
+      );
+    }
   });
 });
