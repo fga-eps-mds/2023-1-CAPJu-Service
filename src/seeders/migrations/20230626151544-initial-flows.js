72ac12d69f10f9c36b8ff6e04ea0e6c9cc8f37d0
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-26 12:32:25 -0300

    fix: Adiciona seed de fluxo
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

---
+++ src/seeders/migrations/20230626151544-initial-flows.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "flow",
+      [
+        {
+          idUnit: 1,
+          name: "Fluxo ABCD",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("flow", null, {});
+  },
+};
