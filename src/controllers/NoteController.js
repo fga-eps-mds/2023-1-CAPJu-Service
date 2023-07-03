3323f7f95d77af82275d1959970ae2693f469629
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2023-07-03 18:01:04 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#185) - Cria método update para Notes

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -51,6 +51,24 @@ class NoteController {
       return res.status(500).json({ message: `Erro ao deletar observação: ${error}` });
     }
   }
+
+  async update(req, res) {
+    const { commentary, } = req.body;
+    const { idNote } = req.params;
+
+    try {
+      const note = await Note.findByPk(idNote);
+      if (!note) {
+        return res.status(400).json({ error: `idNote ${idNote} não existe!` });
+      } else {
+        role.set({ commentary });
+        await role.save();
+        return res.status(200).json({ message: "Observação atualizada com sucesso." });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: `Erro ao atualizar observação: ${error}` });
+    }
+  }
 }
 
 export default new NoteController();
