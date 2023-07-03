043210a1525e107f76384b9c16335f170fb8cffe
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2023-07-02 23:21:08 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#122) - Aumenta cobertura
    Co-authored-by: OscarDeBrito <oscarcorreiadebrito@gmail.com>
    Co-authored-by: YanWerlley <yanwerlley0101@gmail.com>

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,31 +1,28 @@
 import { Op } from "sequelize";
 
 export function filterByNicknameAndRecord(req) {
-  const { filter } = req.query;
-  return filter
+  return req.query.filter
     ? {
         [Op.or]: [
-          { record: { [Op.like]: `%${filter}%` } },
-          { nickname: { [Op.like]: `%${filter}%` } },
+          { record: { [Op.like]: `%${req.query.filter}%` } },
+          { nickname: { [Op.like]: `%${req.query.filter}%` } },
         ],
       }
     : {};
 }
 
 export function filterByName(req) {
-  const { filter } = req.query;
-  return filter
+  return req.query.filter
     ? {
-        [Op.or]: [{ name: { [Op.like]: `%${filter}%` } }],
+        [Op.or]: [{ name: { [Op.like]: `%${req.query.filter}%` } }],
       }
     : {};
 }
 
 export function filterByFullName(req) {
-  const { filter } = req.query;
-  return filter
+  return req.query.filter
     ? {
-        [Op.or]: [{ fullName: { [Op.like]: `%${filter}%` } }],
+        [Op.or]: [{ fullName: { [Op.like]: `%${req.query.filter}%` } }],
       }
     : {};
 }
