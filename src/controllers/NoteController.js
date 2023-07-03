961d00b536a51cd065bf838f5e4ff8481ebe65bc
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2023-07-03 16:08:01 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#185) - Cria método index para Notes

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -1,6 +1,21 @@
 import Note from "../models/Notes.js";
 
-class FlowController {
+class NoteController {
+  index = async (req, res) => {
+    const { record } = req.params;
+    try {
+      const note = await Note.findOne({ where: { record } });
+      if (note) {
+        return res.status(200).json(note);
+      } else {
+        return res
+          .status(204)
+          .json({ message: 'Nenhuma observação encontrada.' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro ao buscar obervação.' });
+    }
+  };
 }
 
-export default new FlowController();
+export default new NoteController();
