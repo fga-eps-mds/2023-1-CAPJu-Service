2a6e66f9061d807e85679d8888343e201e351e54
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-07-08 16:58:30 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#92) - Reescreve todos os testes de usuários

--- src/tests/__tests__/test_user_routes.js
+++ src/tests/__tests__/test_user_routes.js
@@ -417,4 +417,419 @@ describe("user endpoints", () => {
     expect(resMock.status).toHaveBeenCalledWith(500);
   });
 
+  test("updateUser - update user email (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      idUnit: 1,
+      idRole: 1,
+      accepted: true,
+      set: jest.fn(),
+      save: jest.fn(),
+    };
+
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: 1,
+      body: {
+        email: 'novofulano@email.com'
+      }
+    };
+
+    await UserController.updateUser(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Email atualizado com sucesso" });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("updateUser - user not found in update user email (204) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      params: 1,
+      body: {
+        email: 'novofulano@email.com'
+      }
+    };
+
+    await UserController.updateUser(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({});
+    expect(resMock.status).toHaveBeenCalledWith(204);
+  });
+
+  test("updateUser - error in update user (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      params: 1,
+      body: {
+        email: 'novofulano@email.com'
+      }
+    };
+    await UserController.updateUser(reqMock, resMock);
+
+    expect(resMock.json).toHaveBeenCalledWith({
+      error,
+      message: "Impossível atualizar email",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
+  test("updateRole - update user role (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      idUnit: 1,
+      idRole: 1,
+      accepted: true,
+      set: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      body: {
+        cpf: '123456778900',
+        idRole: 3
+      }
+    };
+    await UserController.updateRole(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Papel atualizado com sucesso" });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("updateRole -  user not found in update user role (204) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      body: {
+        cpf: '123456778900',
+        idRole: 3
+      }
+    };
+    await UserController.updateRole(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({});
+    expect(resMock.status).toHaveBeenCalledWith(204);
+  });
+
+
+  test("updateRole - error in update role (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      body: {
+        cpf: '123456778900',
+        idRole: 3
+      }
+    };
+    await UserController.updateRole(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({
+      message: "Usuário não atualizado!",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
+  test("editPassword - update user password (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      password: "123Fulano",
+      idUnit: 1,
+      idRole: 1,
+      accepted: true,
+      set: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        oldPassword: "123Fulano",
+        newPassword: "1234Fulano",
+      }
+    };
+    await UserController.editPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Usuário atualizado com sucesso!" });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("editPassword - user not found in update user password (204) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        oldPassword: "123Fulano",
+        newPassword: "1234Fulano",
+      }
+    };
+    await UserController.editPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({});
+    expect(resMock.status).toHaveBeenCalledWith(204);
+  });
+
+  test("editPassword - invalid password in update user password (204) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      password: "12345Fulano",
+      idUnit: 1,
+      idRole: 1,
+      accepted: true,
+      set: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        oldPassword: "123Fulano",
+        newPassword: "1234Fulano",
+      }
+    };
+    await UserController.editPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Senha inválida!" });
+    expect(resMock.status).toHaveBeenCalledWith(400);
+  });
+
+  test("editPassword - error in upadte user password (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        oldPassword: "123Fulano",
+        newPassword: "1234Fulano",
+      }
+    };
+    await UserController.editPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({
+      error,
+      message: "Erro a atualizar usuário ",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+
+  });
+
+  test("deleteByParam - delete user by cpf (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      password: "123Fulano",
+      idUnit: 1,
+      idRole: 1,
+      accepted: true,
+      destroy: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.deleteByParam(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Usuário apagado com sucesso" });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("deleteByParam - user not found in delete user by cpf (404) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.deleteByParam(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ error: "Usuário não existe!" });
+    expect(resMock.status).toHaveBeenCalledWith(404);
+  });
+
+  test("deleteByParam - error in delete user by cpf (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.deleteByParam(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({
+      error,
+      message: "Erro ao apagar usuário",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
+  test("acceptRequest - accepted user request (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      password: "123Fulano",
+      idUnit: 1,
+      idRole: 1,
+      accepted: false,
+      set: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.acceptRequest(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Usuário aceito com sucesso", });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("acceptRequest - user not found in accepted user request (200) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.acceptRequest(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ error: "Usuário não existe" });
+    expect(resMock.status).toHaveBeenCalledWith(404);
+  });
+
+  test("acceptRequest - user not found in accepted user request (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.acceptRequest(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({
+      error,
+      message: "Falha ao aceitar usuário",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
+  test("deleteRequest - delete user request (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      password: "123Fulano",
+      idUnit: 1,
+      idRole: 1,
+      accepted: false,
+      destroy: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.deleteRequest(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Usuário não aceito foi excluído", });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("deleteRequest - user not found in delete user request (404) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.deleteRequest(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ error: "Usuário não existe" });
+    expect(resMock.status).toHaveBeenCalledWith(404);
+  });
+
+  test("deleteRequest - error in delete user request (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+    };
+    await UserController.deleteRequest(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({
+      error,
+      message: "Erro ao negar pedido do usuário",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
+  test("updateUserEmailAndPassword - update user emain and password (200) ", async () => {
+    const testUser = {
+      fullName: "Fulano De Ciclano",
+      cpf: "123456778900",
+      email: "fulano@email.com",
+      password: "123Fulano",
+      idUnit: 1,
+      idRole: 1,
+      accepted: true,
+      set: jest.fn(),
+      save: jest.fn(),
+    };
+    User.findByPk = jest.fn().mockResolvedValue(testUser);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        email: "fulano@email.com",
+        password: "12345Fulano",
+      }
+    };
+    await UserController.updateUserEmailAndPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith(testUser);
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test("updateUserEmailAndPassword - user not found in update user emain and password (404) ", async () => {
+    User.findByPk = jest.fn().mockResolvedValue();
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        email: "fulano@email.com",
+        password: "12345Fulano",
+      }
+    };
+    await UserController.updateUserEmailAndPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({ message: "Nenhum usuário foi encontrado" });
+    expect(resMock.status).toHaveBeenCalledWith(404);
+  });
+
+  test("updateUserEmailAndPassword - error in update user email and password (500) ", async () => {
+    const error = new Error("Internal Error");
+    User.findByPk = jest.fn().mockRejectedValue(error);
+    reqMock = {
+      params: {
+        id: '123456778900',
+      },
+      body: {
+        email: "fulano@email.com",
+        password: "12345Fulano",
+      }
+    };
+    await UserController.updateUserEmailAndPassword(reqMock, resMock);
+    expect(resMock.json).toHaveBeenCalledWith({
+      error,
+      message: "Impossível atualizar email",
+    });
+    expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
 });
